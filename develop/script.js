546f05a5003d7a895ba52d6e9d3464af37c3459c
Evelyn Goris <goris.evelyn@gmail.com>
2020-09-15 06:56:31 -0500

    pseudocoded script.js, added apiKey

--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,22 @@
 console.log("Weather Dashboard");
 
+apiKey = "96404ebac51d984e233fe3941651e4ab"
+
+// present current and future conditions for a city that is searched 
+
+// add that city to the search history
+
+// view current weather conditions for that city
+// Show current city name, date, icon representation of weather conditions, temperature, humidity, wind speed, and UV index.
+
+
+// Show UV index with a color that indicates whether the conditions are favorable, moderate, or severe.
+
+// Show a 5-day forecast (future weather conditions for that city) that displays  date,  icon representation of weather conditions,  temperature, and  humidity.
+
+// Show current and future conditions for the new city searched.
+
+// Store last searched city forecast.
+
+
 
